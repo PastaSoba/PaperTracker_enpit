46deb17e0a348e709adb1cfa1aee012976a6a5ec
PastaSoba <sanukinomiyatuko09@gmail.com>
2020-11-18 11:28:53 +0900

    デモページの記述を削除した

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Paper Tracker</h1>
+        <input type="search" name="paper name" placeholder="調べたい論文名を入力"/>
+        <button type="button" class="btn-search">検索</button>
       </header>
     </div>
   );
