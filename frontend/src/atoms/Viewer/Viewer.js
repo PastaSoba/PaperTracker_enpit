402b737fd2c7824f9eedab32f7fa89383171d2f4
PastaSoba <sanukinomiyatuko09@gmail.com>
2020-12-04 16:00:55 +0900

    [変更] 論文の発行年の表記を独立して行えるようにした #1

--- frontend/src/atoms/Viewer/Viewer.js
+++ frontend/src/atoms/Viewer/Viewer.js
@@ -2,21 +2,49 @@ import React, { useState } from "react"
 import { Stage, Layer, Rect, Text, Group } from 'react-konva';
 import Konva from 'konva';
 
-function ColordRect(props) {
+
+const RECT_WIDTH = 200;  // 格子の横幅
+const RECT_HEIGHT = 50;  // 格子の縦幅
+const RECT_MARGIN = 5;   // 格子間の距離
+
+function YearRect(props) {
+  return (
+    <Group>
+      <Rect
+        x={(RECT_WIDTH+RECT_MARGIN)*(props.col+1)}
+        y={(RECT_HEIGHT+RECT_MARGIN)*props.row}
+        width={RECT_WIDTH}
+        height={RECT_HEIGHT}
+      />
+      <Text
+        x={(RECT_WIDTH+RECT_MARGIN)*(props.col+1)}
+        y={(RECT_HEIGHT+RECT_MARGIN)*props.row}
+        width={RECT_WIDTH}
+        height={RECT_HEIGHT}
+        text={props.year}
+        fontSize={20}
+        align={"center"}
+        verticalAlign={"middle"}
+        />
+    </Group>
+  )
+}
+
+function PaperRect(props) {
     return (
       <Group>
         <Rect
-          x={205*(props.col+1)}
-          y={55*props.row}
-          width={200}
-          height={50}
+          x={(RECT_WIDTH+RECT_MARGIN)*(props.col+1)}
+          y={(RECT_HEIGHT+RECT_MARGIN)*props.row}
+          width={RECT_WIDTH}
+          height={RECT_HEIGHT}
           fill={props.color}
         />
         <Text
-          x={205*(props.col+1)}
-          y={55*props.row}
-          width={200}
-          height={50}
+          x={(RECT_WIDTH+RECT_MARGIN)*(props.col+1)}
+          y={(RECT_HEIGHT+RECT_MARGIN)*props.row}
+          width={RECT_WIDTH}
+          height={RECT_HEIGHT}
           text={props.title}
           />
       </Group>
@@ -39,24 +67,21 @@ class Viewer extends React.Component {
           let citations = this.props.citations;
           for(let i=0; i<citations.length; i++) {
             if(i===0){
-              // Doing None
+              items.push(
+                <YearRect year={citations[i]["year"]} row={row} col={-1}/>
+              );
             } else if(citations[i]["year"] === citations[i-1]["year"]) {
               col++                // 列を1つ左にずらす
             } else {
               col = 0; row++;      // 行を1つ増やす
+              items.push(
+                <YearRect year={citations[i]["year"]} row={row} col={-1}/>
+              );
             }
-            items.push(
-              // 発行年を表すyearRect
-              <ColordRect 
-                title={citations[i]["year"]}
-                row={row}
-                col={-1}
-                color={'white'}
-              />
-            );
+            
             items.push(
               // 各論文データ
-              <ColordRect 
+              <PaperRect 
                 title={citations[i]["title"]}
                 row={row}
                 col={col}
@@ -72,23 +97,19 @@ class Viewer extends React.Component {
           col = 0; row+=2;
           for(let i=0; i<references.length; i++) {
             if(i===0){
-              // Doing None
+              items.push(
+                <YearRect year={citations[i]["year"]} row={row} col={-1}/>
+              );
             } else if(references[i]["year"] === references[i-1]["year"]) {
               col++                // 列を1つ左にずらす
             } else {
               col = 0; row++;      // 行を1つ増やす
+              items.push(
+                <YearRect year={citations[i]["year"]} row={row} col={-1}/>
+              );
             }
             items.push(
-              // 発行年を表すyearRect
-              <ColordRect 
-                title={references[i]["year"]}
-                row={row}
-                col={-1}
-                color={'white'}
-              />
-            );
-            items.push(
-              <ColordRect 
+              <PaperRect 
                 title={references[i]["title"]}
                 row={row}
                 col={col}
